1d7c8ba1f8bf39d5e7353b065d98901d0fd7b459
wuwf <272551766@qq.com>
2018-04-25 11:45:54 +0800

    1

--- src/main/java/com/jnrise/server/model/Channel.java
+++ src/main/java/com/jnrise/server/model/Channel.java
@@ -16,6 +16,16 @@ public class Channel {
 
     private String channel;
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
