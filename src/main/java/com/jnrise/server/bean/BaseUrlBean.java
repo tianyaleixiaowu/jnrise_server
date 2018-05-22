3210ffea252622e9147808459574eaaccab0e552
wuwf <272551766@qq.com>
2018-05-22 16:15:24 +0800

    1

--- src/main/java/com/jnrise/server/bean/BaseUrlBean.java
+++ src/main/java/com/jnrise/server/bean/BaseUrlBean.java
@@ -21,11 +21,11 @@ public class BaseUrlBean {
 
     public static void main(String[] args) {
         BaseUrlBean baseUrlBean = new BaseUrlBean();
-        baseUrlBean.setChannel("test");
+        baseUrlBean.setChannel("jnrise");
         baseUrlBean.setOriginal("0531");
-        baseUrlBean.setType1(0 + "");
+        baseUrlBean.setType1(1 + "");
         //for (int i = 1; i <= 10; i++) {
-            baseUrlBean.setType2(0 + "");
+            baseUrlBean.setType2(1 + "");
             System.out.println("https://mm.jnrise.cn:88/loading/jtao11xi.html?sign=" + baseUrlBean.encrypt());
         //}
 
--- src/main/java/com/jnrise/server/model/Enter.java
+++ src/main/java/com/jnrise/server/model/Enter.java
@@ -13,4 +13,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "enter")
 public class Enter extends BaseEntity {
+    private byte flag;
+
+    public byte getFlag() {
+        return flag;
+    }
+
+    public void setFlag(byte flag) {
+        this.flag = flag;
+    }
 }
