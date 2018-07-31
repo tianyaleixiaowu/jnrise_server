f972de6d8cdf8360254d917b809b437979512a52
wuwf <272551766@qq.com>
2018-07-31 16:57:12 +0800

    click加上module stay

--- src/main/java/com/jnrise/server/controller/ModuleStayController.java
+++ src/main/java/com/jnrise/server/controller/ModuleStayController.java
@@ -1,10 +1,25 @@
 package com.jnrise.server.controller;
 
+import com.jnrise.server.bean.BaseData;
+import com.jnrise.server.manager.ModuleStayManager;
+import com.jnrise.server.model.ModuleStay;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author wuweifeng wrote on 2018/7/31.
  */
 @RestController
+@RequestMapping("/moduleStay")
 public class ModuleStayController {
+    @Resource
+    private ModuleStayManager moduleStayManager;
+
+    @RequestMapping
+    public BaseData add(ModuleStay stay) {
+        moduleStayManager.add(stay);
+        return new BaseData(0, "");
+    }
 }
