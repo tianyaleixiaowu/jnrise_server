02a36a4026222f8ba8870b28282d7c4e0891d6f2
wuwf <272551766@qq.com>
2018-08-09 17:27:38 +0800

    url加密

--- src/main/java/com/jnrise/server/controller/UrlController.java
+++ src/main/java/com/jnrise/server/controller/UrlController.java
@@ -13,28 +13,12 @@ public class UrlController {
 
 
     @RequestMapping("")
-    public String encode(String data) {
-        String[] array = data.split("\n");
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String ss : array) {
-            stringBuilder.append(deal(ss)).append("\n");
-        }
+    public String encode(BaseUrlBean baseUrlBean) {
 
-        return stringBuilder.toString();
+        return deal(baseUrlBean);
     }
 
-    private static String deal(String s) {
-        String[] array = s.replace("channel=", "")
-                .replace("original=", "")
-                .replace("type1=", "")
-                .replace("type2=", "").split("&");
-
-
-        BaseUrlBean baseUrlBean = new BaseUrlBean();
-        baseUrlBean.setChannel(array[0]);
-        baseUrlBean.setOriginal(array[1]);
-        baseUrlBean.setType1(array[2]);
-        baseUrlBean.setType2(array[3]);
+    private static String deal(BaseUrlBean baseUrlBean) {
         String ss = "https://mm.jnrise.cn/loading/r1oxd5.html?sign=" + baseUrlBean.encrypt();
         System.out.println(ss);
         return ss;
