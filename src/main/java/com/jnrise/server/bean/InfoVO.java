ef588f26a411c18c743c99d377bd44728b965470
wuwf <272551766@qq.com>
2019-04-22 10:07:37 +0800

    company选择，查询功能

--- src/main/java/com/jnrise/server/bean/InfoVO.java
+++ src/main/java/com/jnrise/server/bean/InfoVO.java
@@ -24,6 +24,15 @@ public class InfoVO {
      * 校区
      */
     private String school;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public String getPhone() {
         return phone;
